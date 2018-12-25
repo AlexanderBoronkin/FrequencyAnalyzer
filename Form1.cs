655aa9e0bb1b8869cb007472c4d6d44ccd4f85f7
Alexander Boronkin <boronkin.alex.under@gmail.com>
2018-12-25 19:18:00 +0700

    Done!

--- Form1.cs
+++ Form1.cs
@@ -69,13 +69,13 @@ namespace FrequencyAnalyzer
             {
                 MessageBox.Show("Ошибка при чтении файла");
             }
+            all_data = richTextBox1.Text;
         }
 
 
         //анализ
         private void button2_Click(object sender, EventArgs e)
         {
-            all_data = richTextBox1.Text;
             //считаем частоту каждого символа
             for (int i = 0; i < all_data.Length; i++)
             {
@@ -89,34 +89,77 @@ namespace FrequencyAnalyzer
             for (int i = 0; i < freq.Length; i++)
                 freq[i] = freq[i] * 100 / all_data.Length;
 
-            double[] tab_freq_sort = tab_freq;
-            double[] freq_sort = freq;
+            double[] tab_freq_sort = new double [tab_freq.Length];
+            for (int i = 0; i < tab_freq_sort.Length; i++)
+                tab_freq_sort[i] = tab_freq[i];
+            double[] freq_sort = new double[freq.Length];
+            for (int i = 0; i < freq_sort.Length; i++)
+                freq_sort[i] = freq[i];
+            char[] alph_1 = new char[alphabet.Length];
+            for (int i = 0; i < alph_1.Length; i++)
+                alph_1[i] = alphabet[i];
+            char[] alph_2 = new char[alphabet.Length];
+            for (int i = 0; i < alph_2.Length; i++)
+                alph_2[i] = alphabet[i];
 
             for (int i = 0; i < tab_freq.Length; i++)
             {
                 for (int j = tab_freq.Length - 1; j > i; j--)
                 {
-                    if (tab_freq_sort[j - 1] > tab_freq_sort[j])
+                    if (tab_freq_sort[j - 1] < tab_freq_sort[j])
                     {
                         double x = tab_freq_sort[j - 1];
                         tab_freq_sort[j - 1] = tab_freq_sort[j];
                         tab_freq_sort[j] = x;
+                        char ch = alph_1[j - 1];
+                        alph_1[j - 1] = alph_1[j];
+                        alph_1[j] = ch;
                     }
-                    if (freq_sort[j - 1] > freq_sort[j])
+                    if (freq_sort[j - 1] < freq_sort[j])
                     {
                         double x = freq_sort[j - 1];
                         freq_sort[j - 1] = freq_sort[j];
                         freq_sort[j] = x;
+                        char ch = alph_2[j - 1];
+                        alph_2[j - 1] = alph_2[j];
+                        alph_2[j] = ch;
                     }
                 }
             }
-            ;
+            alph_mod = new char[alphabet.Length];
+            //сама функция подстановки 
+            for(int i=0;i<alphabet.Length;i++)
+            {
+                for(int j=0;j<alphabet.Length;j++)
+                    if(alph_1[i]==alphabet[j])
+                    {
+                        alph_mod[j] = alph_2[i];
+                    }
+            }
+            ToTextBoxes();
+            MessageBox.Show("Ну все, дальше сами. Ручками.");
         }
 
         //обновить
         private void button3_Click(object sender, EventArgs e)
         {
-            all_data = richTextBox1.Text;
+            MakeKey();
+            string new_data = "";
+            string data = all_data.ToLowerInvariant();
+            for (int i = 0; i < data.Length; i++)
+            {
+                char c = data[i];
+                for (int j = 0; j < alph_mod.Length; j++)
+                {
+                    if (c == alph_mod[j])
+                    {
+                        c = alphabet[j];
+                        break;
+                    }
+                }
+                new_data += c;
+            }
+            richTextBox1.Text = new_data.ToLowerInvariant();
         }
 
         //сохранить в файл
@@ -138,9 +181,8 @@ namespace FrequencyAnalyzer
             }
         }
 
-        private void Make_key()
+        private void MakeKey()
         {
-            alph_mod = new char[alphabet.Length];
             alph_mod[0] = Convert.ToChar(textА.Text); alph_mod[1] = Convert.ToChar(textБ.Text);
             alph_mod[2] = Convert.ToChar(textВ.Text); alph_mod[3] = Convert.ToChar(textГ.Text);
             alph_mod[4] = Convert.ToChar(textД.Text); alph_mod[5] = Convert.ToChar(textЕ.Text);
@@ -157,6 +199,26 @@ namespace FrequencyAnalyzer
             alph_mod[26] = Convert.ToChar(textЪ.Text); alph_mod[27] = Convert.ToChar(textЫ.Text);
             alph_mod[28] = Convert.ToChar(textЬ.Text); alph_mod[29] = Convert.ToChar(textЭ.Text);
             alph_mod[30] = Convert.ToChar(textЮ.Text); alph_mod[31] = Convert.ToChar(textЯ.Text);
+        }
+
+        private void ToTextBoxes()
+        {
+            textА.Text = Convert.ToString(alph_mod[0]);textБ.Text = Convert.ToString(alph_mod[1]);
+            textВ.Text = Convert.ToString(alph_mod[2]); textГ.Text = Convert.ToString(alph_mod[3]);
+            textД.Text = Convert.ToString(alph_mod[4]); textЕ.Text = Convert.ToString(alph_mod[5]);
+            textЖ.Text = Convert.ToString(alph_mod[6]); textЗ.Text = Convert.ToString(alph_mod[7]);
+            textИ.Text = Convert.ToString(alph_mod[8]); textЙ.Text = Convert.ToString(alph_mod[9]);
+            textК.Text = Convert.ToString(alph_mod[10]); textЛ.Text = Convert.ToString(alph_mod[11]);
+            textМ.Text = Convert.ToString(alph_mod[12]); textН.Text = Convert.ToString(alph_mod[13]);
+            textО.Text = Convert.ToString(alph_mod[14]); textП.Text = Convert.ToString(alph_mod[15]);
+            textР.Text = Convert.ToString(alph_mod[16]); textС.Text = Convert.ToString(alph_mod[17]);
+            textТ.Text = Convert.ToString(alph_mod[18]); textУ.Text = Convert.ToString(alph_mod[19]);
+            textФ.Text = Convert.ToString(alph_mod[20]); textХ.Text = Convert.ToString(alph_mod[21]);
+            textЦ.Text = Convert.ToString(alph_mod[22]); textЧ.Text = Convert.ToString(alph_mod[23]);
+            textШ.Text = Convert.ToString(alph_mod[24]); textЩ.Text = Convert.ToString(alph_mod[25]);
+            textЪ.Text = Convert.ToString(alph_mod[26]); textЫ.Text = Convert.ToString(alph_mod[27]);
+            textЬ.Text = Convert.ToString(alph_mod[28]); textЭ.Text = Convert.ToString(alph_mod[29]);
+            textЮ.Text = Convert.ToString(alph_mod[30]); textЯ.Text = Convert.ToString(alph_mod[31]);
         }
 
     }
